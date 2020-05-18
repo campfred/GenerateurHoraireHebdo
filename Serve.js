66a9795b2fc3fe45c4f172b01209a634d93f2407
Camp <camp.fred@hotmail.com>
2020-05-18 01:07:53 -0400

    Tentative de correction de port

--- Serve.js
+++ Serve.js
@@ -1,6 +1,14 @@
+// const HTTP = require("http")
+
+// HTTP.createServer((request, response) => {
+// 	response.sendFile("Horaire.html")
+// 	response.end
+// }).listen(process.env.PORT)
+
+
 const EXPRESS = require("express")
 const APP = EXPRESS()
-const PORT = process.env.port
+const PORT = process.env.PORT
 
 APP.get("/", (request, response) => {
 	response.sendFile("Horaire.html")
