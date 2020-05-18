2cb2f3df9f2b2ea12e58988118d0e48043765aeb
Camp <camp.fred@hotmail.com>
2020-05-18 00:53:24 -0400

    Préparation pour Heroku
    Ajout de fichiers nécessaires pour faire rouler sur Heroku.

---
+++ Serve.js
@@ -0,0 +1,8 @@
+const EXPRESS = require("express")
+const APP = express()
+
+applicationCache.get("/", (request, response) => {
+	response.sendFile("Horaire.html")
+})
+
+applicationCache.listen(process.env.port)
